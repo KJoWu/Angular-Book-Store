339ef77fb1077a17c3bf924454fbcd4e43613fde
Silver Rook <kwu1@ualberta.ca>
2015-10-22 17:26:49 -0600

    added more directives/expressions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 (function() {
     var app = angular.module('store', []); /* making a module named store */
     app.controller("StoreController", function() {
-        this.product = book;
+        this.products = books;
     });
 
-    var book = {
+    var books = [{
         name: "Harry Potter",
         price: 20,
         description: "It's a Book, Harry",
-    };
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: "The Mist",
+        price: 15,
+        description: "A scary misty story",
+        canPurchase: true,
+        soldOut: false
+    }, {
+        name: "The Red Fox",
+        price: 10,
+        description: "It's a fox story",
+        canPurchase: true,
+        soldOut: false
+    }];
 })();
