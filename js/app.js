0bef9bf651c2e4a5c28a3b65f5c10cc94734d0f0
Silver Rook <kwu1@ualberta.ca>
2015-10-22 17:17:32 -0600

    added basic directive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 (function() {
-
     var app = angular.module('store', []); /* making a module named store */
+    app.controller("StoreController", function() {
+        this.product = book;
+    });
 
+    var book = {
+        name: "Harry Potter",
+        price: 20,
+        description: "It's a Book, Harry",
+    };
 })();
